b722f476fcbb46a6a9243d1fc6a87e8a5e0555e6
CptResonable <adolfssonalbin.94@gmail.com>
2023-04-25 14:11:14 +0200

    Found and fixed the bug with VFX not showing

--- Assets/Misc scripts/Despawner.cs
+++ Assets/Misc scripts/Despawner.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 using EZ_Pooling;
 
 public class Despawner : MonoBehaviour {
-    private Coroutine currentDespawnRutine;
+    //private Coroutine currentDespawnRutine;
+
+    //public void DelayedDespawn(float delaySec) {
+    //    if (currentDespawnRutine != null)
+    //        StopCoroutine(currentDespawnRutine);
+
+    //    currentDespawnRutine = StartCoroutine(_delayedDespawn(delaySec));
+    //}
+
+    //private IEnumerator _delayedDespawn(float delaySec) {
+    //    yield return new WaitForSeconds(delaySec);
+    //    EZ_PoolManager.Despawn(transform);
+    //    yield return null;
+    //}
 
     public void DelayedDespawn(float delaySec) {
-        if (currentDespawnRutine != null)
-            StopCoroutine(currentDespawnRutine);
-
-        currentDespawnRutine = StartCoroutine(_delayedDespawn(delaySec));
+        Utils.DelayedFunctionCall(Despawn, delaySec);
     }
 
-    private IEnumerator _delayedDespawn(float delaySec) {
-        yield return new WaitForSeconds(delaySec);
+    private void Despawn() {
         EZ_PoolManager.Despawn(transform);
-        yield return null;
     }
 }
